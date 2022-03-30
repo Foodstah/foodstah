619086632a1009725e3b0fc27225ec1602012f43
temporaryadmin <danjlambert95@gmail.com>
2022-03-30 19:07:07 +0200

    crated index view to the home page template and started to link static images in the template

--- foodstah/base/urls.py
+++ foodstah/base/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index')
     # ... the rest of your URLconf goes here ...
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
