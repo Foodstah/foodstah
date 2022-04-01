2ef613a1b57869ecb396b28c385ce6018878f0bc
Raphaela <90319461+raphaelavergud@users.noreply.github.com>
2022-04-01 10:58:36 +0200

    Update urls.py

--- foodstah/user/urls.py
+++ foodstah/user/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("signup/", views.signup, name="signup"),
-    path("profile/<username>/", views.profile_page, name="profile-page")
+    path("profile/<username>/", views.profile_page, name="profile-page"),
     path("login/", views.login, name="login"),
 ]
