f1aadc68e5c6aac24da8d4a71910e5c3dde88bc3
CamilaSiede <86715987+CamilaSiede@users.noreply.github.com>
2022-04-04 00:10:17 +0200

    create post view and add post view

--- foodstah/post/views.py
+++ foodstah/post/views.py
@@ -1,29 +1,24 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import Post
 from .forms import NewPostForm
-from django.contrib import messages
 
 # Create your views here.
 
-
 def post(request):
-    context = {}
-    return render(request, "post/food-feed.html")
-    # if request.user.is_authenticated():
-    #     return render(request, 'post/food-feed.html')
-    # else:
-    #     return render(request, 'user/signup.html')
+    posts = Post.objects.all().order_by('-date_created')
+    return render(request, 'post/food-feed.html', {'posts':posts})
+
 
 def add_post(request):
-    if request.method == "POST":
-        form = NewPostForm(request.POST, logged_in_user=request.user)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Your post was successfully added.")
-            return redirect("/")
-        messages.error(request, "Unsuccessful. Could not add new post.")
     form = NewPostForm()
-    return render(
-        request=request,
-        template_name="post/add_post.html",
-        context={"addpost_form": form},
-    )
+
+    if request.method == 'POST':
+       form = NewPostForm(request.POST, request.FILES)
+
+       if form.is_valid():
+           form.save()
+           return HttpResponseRedirect('food-feed')
+
+    return render(request,'post/add_post.html', {'form':form})
+    
