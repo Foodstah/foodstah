da7fcc4a858d8ee759e0ddd774947fa0a409ca02
Raphaela Vergud <raphaelavergud@icloud.com>
2022-04-04 09:43:22 +0200

    fixed views & template

--- foodstah/post/views.py
+++ foodstah/post/views.py
@@ -1,29 +1,25 @@
 from django.shortcuts import render, redirect
 from .forms import NewPostForm
+from .models import Post
 from django.contrib import messages
 
 # Create your views here.
 
-
 def post(request):
-    context = {}
-    return render(request, "post/food-feed.html")
-    # if request.user.is_authenticated():
-    #     return render(request, 'post/food-feed.html')
-    # else:
-    #     return render(request, 'user/signup.html')
+    posts = Post.objects.all().order_by('-date_created')
+    return render(request, 'post/food-feed.html', {'posts':posts})
 
 def add_post(request):
+
     if request.method == "POST":
         form = NewPostForm(request.POST, logged_in_user=request.user)
+
         if form.is_valid():
             form.save()
             messages.success(request, "Your post was successfully added.")
-            return redirect("/")
+            return redirect("/food-feed")
+
         messages.error(request, "Unsuccessful. Could not add new post.")
+
     form = NewPostForm()
-    return render(
-        request=request,
-        template_name="post/add_post.html",
-        context={"addpost_form": form},
-    )
+    return render(request, "post/add_post.html", {"addpost_form": form})
