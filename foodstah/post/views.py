e03b14a7c2d849abe40a7b63697bcacd6ccf542a
Raphaela <90319461+raphaelavergud@users.noreply.github.com>
2022-04-04 09:44:47 +0200

    Update views.py

--- foodstah/post/views.py
+++ foodstah/post/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from .models import Post
+from django.shortcuts import render, redirect
 from .forms import NewPostForm
+from .models import Post
+from django.contrib import messages
 
 # Create your views here.
 
@@ -9,16 +9,17 @@ def post(request):
     posts = Post.objects.all().order_by('-date_created')
     return render(request, 'post/food-feed.html', {'posts':posts})
 
-
 def add_post(request):
+
+    if request.method == "POST":
+        form = NewPostForm(request.POST, logged_in_user=request.user)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your post was successfully added.")
+            return redirect("/food-feed")
+
+        messages.error(request, "Unsuccessful. Could not add new post.")
+
     form = NewPostForm()
-
-    if request.method == 'POST':
-       form = NewPostForm(request.POST, request.FILES)
-
-       if form.is_valid():
-           form.save()
-           return HttpResponseRedirect('food-feed')
-
-    return render(request,'post/add_post.html', {'form':form})
-    
+    return render(request, "post/add_post.html", {"addpost_form": form})
