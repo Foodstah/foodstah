fa91da79ba615edaabb79d45580c673bf8363ead
temporaryadmin <danjlambert95@gmail.com>
2022-04-04 09:50:02 +0200

    added .lower() function to username singup and login forms so all usernames are lowercase and unique

--- foodstah/user/urls.py
+++ foodstah/user/urls.py
@@ -3,13 +3,14 @@ from user import views as user_view
 from django.contrib.auth import views as auth_view
 from django.conf import settings
 from django.conf.urls.static import static
+from user.forms import MyAuthenticationForm
 
 urlpatterns = [
     path("", user_view.home, name="home"),
     path("signup/", user_view.signup, name="signup"),
     path(
         "login/",
-        auth_view.LoginView.as_view(template_name="user/login.html"),
+        auth_view.LoginView.as_view(form_class=MyAuthenticationForm,template_name="user/login.html"),
         name="login",
     ),
     path(
