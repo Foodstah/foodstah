740d3e45a285e4e9050a26dc09cb60df047521cd
temporaryadmin <danjlambert95@gmail.com>
2022-04-11 16:38:11 +0200

    SEO updated the website for better seo

--- foodstah/post/urls.py
+++ foodstah/post/urls.py
@@ -6,11 +6,8 @@ urlpatterns = [
     path("add-post/", views.add_post, name="add-post"),
     path("likes/<int:pk>/", views.give_like, name="give-like"),
     path("loves/<int:pk>/", views.give_love, name="give-love"),
-    path("droolingface/<int:pk>/", views.give_drooling_face, name="give-drooling-face"),
-    
+    path("droolingface/<int:pk>/", views.give_drooling_face, name="give-drooling-face"), 
     path("post/<slug:slug>/", views.PostDetailsView.as_view(), name="post-detail"),
-
-
     path("post/<int:pk>/update", views.PostUpdateView.as_view(), name="post-update"),
     path("post/<int:pk>/delete", views.PostDeleteView.as_view(), name="post-delete"),
     path("post/<int:pk>/download-recipe", views.recipe_to_pdf, name="download-recipe"),
