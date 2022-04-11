f3c5ec2fbfa9d86e3fcabd238a3fc7d8083bc348
CamilaSiede <86715987+CamilaSiede@users.noreply.github.com>
2022-04-11 21:16:08 +0200

    fix bug in the saved post function

--- foodstah/post/views.py
+++ foodstah/post/views.py
@@ -91,7 +91,7 @@ def save_post(request,pk):
         post.favorite_posts.add(request.user)
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
-def saved_post(request):
+def saved_post(request,username):
     user_saved_post = Post.objects.filter(favorite_posts=request.user)
     return render(request, "post/saved_post.html", {'user_saved_post':user_saved_post})
 
