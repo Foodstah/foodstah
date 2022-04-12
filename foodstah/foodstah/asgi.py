a68a960059330138063bae70bfdb38deae3b82ec
Raphaela Vergud <raphaelavergud@icloud.com>
2022-04-12 14:43:31 +0200

    ASGI change

--- foodstah/foodstah/asgi.py
+++ foodstah/foodstah/asgi.py
@@ -7,18 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
-# import os
-
-# from django.core.asgi import get_asgi_application
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foodstah.settings")
-
-# application = get_asgi_application()
-
 import os
-import django
-from channels.routing import get_default_application
+
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foodstah.settings")
-django.setup()
-application = get_default_application()
+
+application = get_asgi_application()
+
+# import os
+# import django
+# from channels.routing import get_default_application
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foodstah.settings")
+# django.setup()
+# application = get_default_application()
