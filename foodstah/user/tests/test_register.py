65a3cb78a001e6917353a2d84a8d12d6c09957f3
Raphaela Vergud <raphaelavergud@icloud.com>
2022-04-20 22:24:30 +0200

    1 functional test

--- foodstah/user/tests/test_register.py
+++ foodstah/user/tests/test_register.py
@@ -5,55 +5,42 @@ from django.test import TestCase
 from django.urls import reverse
 
 
-class ViewBaseTest(TestCase):
-    def setUp(self):
-        self.register_url = reverse("signup")
-        return super().setUp()
-
-
-class SignUpViewTest(ViewBaseTest):
-    def test_can_view_registerpage_correctly(self):
-        response = self.client.get(self.register_url)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "user/signup.html")
-
-
 class ModelBaseTest(TestCase):
     def setUp(self):
         self.user = {
-            "username": "Tester McTestsalot",
-            "first_name": "Tester",
-            "last_name": "McTestsalot",
+            "username": "Raphiie",
             "email": "test@test.com",
-            "password": "test",
+            "password": "se",
         }
 
 
 class SignUpModelTest(ModelBaseTest):
-    def test_value_error_when_email_is_none(self):
-        with self.assertRaises(ValueError):
-            self.user["email"] = None
-            get_user_model().objects.create_user(**(self.user))
 
-    def test_value_error_when_email_is_empty(self):
-        with self.assertRaises(ValueError):
-            self.user["email"] = ""
-            get_user_model().objects.create_user(**(self.user))
+    def test_user_registration(self):
+        user = get_user_model().objects.create_user(**(self.user))
+        self.assertEquals(user.username, self.user["username"])
+        self.assertEquals(user.email, self.user["email"])
+        self.assertTrue(user.check_password(self.user["password"]))
+        self.assertFalse(user.is_superuser)
+        self.assertFalse(user.is_staff)
+        self.assertTrue(user.is_active)
 
-    def test_email_passes_with_standard_email_address(self):
-        with self.assertRaises(ValueError):
-            self.user["email"] = "test@test.com"
-            get_user_model().objects.create_user(**(self.user))
 
-#     def test_user_registration(self):
-#         user = get_user_model().objects.create_user(**(self.user))
-#         self.assertEquals(user.username, self.user["username"])
-#         self.assertEquals(user.first_name, self.user["first_name"])
-#         self.assertEquals(user.email, self.user["email"])
-#         self.assertTrue(user.check_password(self.user["password"]))
-#         self.assertFalse(user.is_superuser)
-#         self.assertFalse(user.is_staff)
-#         self.assertTrue(user.is_active)
+    # def test_value_error_when_email_is_none(self):
+    #     with self.assertRaises(ValueError):
+    #         self.user["email"] = None
+    #         get_user_model().objects.create_user(**(self.user))
+
+    # def test_value_error_when_email_is_empty(self):
+    #     with self.assertRaises(ValueError):
+    #         self.user["email"] = ""
+    #         get_user_model().objects.create_user(**(self.user))
+
+    # def test_email_passes_with_standard_email_address(self):
+    #     with self.assertRaises(ValueError):
+    #         self.user["email"] = "test@test.com"
+    #         get_user_model().objects.create_user(**(self.user))
+
 
 
 # class RegisterSuperuserModelTest(ModelBaseTest):
